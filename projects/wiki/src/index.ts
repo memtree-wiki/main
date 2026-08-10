da6bd2c2e17343a9d18a04ec8fc4b40cd09d2688
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-08-10 15:56:57 +0300

    fix

--- projects/wiki/src/index.ts
+++ projects/wiki/src/index.ts
@@ -1,1 +1,11 @@
-type embedder = (doc: Document)
+import type { Document } from "@memtree.wiki/docs"
+
+type Vector = readonly number[]
+
+type Embedder = (doc: Document) => Vector
+
+interface Params {
+  embedder: Embedder
+}
+
+export function memTree({ embedder }: Params) { }
