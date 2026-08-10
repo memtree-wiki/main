ba2afb2243b73c54badc75095f24cfe588b733c2
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-08-10 15:53:37 +0300

    fix

--- projects/wiki/src/index.ts
+++ projects/wiki/src/index.ts
@@ -1,7 +1,1 @@
-export function store(document: unknown): void {
-  throw new Error("Not implemented");
-}
-
-export function retrieve(id: string): unknown {
-  throw new Error("Not implemented");
-}
+type embedder = (doc: Document)
