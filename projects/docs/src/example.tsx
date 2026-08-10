6bb4124c68e503186672c1b8d7826fd307bd1c3e
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-08-10 10:33:16 +0300

    fix

---
+++ projects/docs/src/example.tsx
@@ -0,0 +1,12 @@
+
+import type { Document } from "."
+
+const doc: Document = {
+    //  TODO: generate a complete document using the Document schema, make sure to use all available elements. Make a short document that demonstrates the capabilities of the Document schema.
+
+}
+
+
+if (import.meta.main) {
+    // TODO: output a full html with embedded css. The document should be optimized for ease of reading.
+}
