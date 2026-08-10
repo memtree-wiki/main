878d8a50a5a4158d4fd7953a1a4a3866a1493d3a
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-08-10 09:29:40 +0300

    init

---
+++ projects/wiki/src/index.ts
@@ -0,0 +1,7 @@
+export function store(document: unknown): void {
+  throw new Error("Not implemented");
+}
+
+export function retrieve(id: string): unknown {
+  throw new Error("Not implemented");
+}
