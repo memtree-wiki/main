4be3889610fd19072c2d6cef22847e3406b20f62
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-08-11 07:19:03 +0300

    fix

--- projects/wiki/src/index.ts
+++ projects/wiki/src/index.ts
@@ -4,10 +4,47 @@ type Vector = readonly number[]
 
 type Embedder = (doc: Document) => Promise<Vector>
 type Merger = (docs: [Document, Document]) => Document
+type Splitter = (doc: Document) => {
+  parent: Document
+  children: Document[]
+}
 
 interface Params {
   embedder: Embedder
   merger: Merger
+  splitter: Splitter
 }
 
-export function memTree({ embedder, merger }: Params) { }
+type API = ReturnType<typeof memTree>
+
+export function memTree({ embedder, merger, splitter }: Params) {
+  // SEARCH
+  interface SearchParams {
+    query: string
+  }
+
+  function search({ }: SearchParams) { }
+
+  // READ
+  interface ReadParams {
+    nodeId: number
+  }
+
+  function read({ nodeId }: ReadParams) { }
+
+  // ADD
+  interface AddParams {
+    doc: Document
+  }
+
+  function add({ doc }: AddParams) { }
+
+  // DELETE
+  interface DelParams {
+    subtreeId: number
+  }
+
+  function del({ subtreeId }: DelParams) { }
+
+  return { search, read, add, del }
+}
